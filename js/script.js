6bd76b3385fd4150f4e7a4c392eabcc279d7ef67
Soro Cat <sorousha326@gmail.com>
2019-07-03 14:24:21 -0700

    add changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 var roman = function(sentence){
   var numerals = ["I","V","X","L","C","D","M"]
+  var number = ["1","5","10","50","100","500","1000"]
+  // var splitstring = sentence.split("");
+  var newWord;
+  for(x=0; x <   )
+   if (splitstring[0]===letter){
+
 
 // user interface logic
 $(document).ready(function() {
