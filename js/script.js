eb6740fdd009f84223d44deaa53bd20f2a8a987d
Soro Cat <sorousha326@gmail.com>
2019-07-08 10:18:26 -0700

    add changes

--- js/script.js
+++ js/script.js
@@ -3,11 +3,14 @@ var roman = function(sentence){
   var number = ["1","5","10","50","100","500","1000"]
   // var splitstring = sentence.split("");
   var newWord;
-  for(x=0; x < splitstring.length; x++){  
+  for(x=0; x < splitstring.length; x++){
     numerals.forEach(function(letter){
      if (splitstring[0]===letter){
-
-
+       console.log(x);
+     });
+    };
+  }
+    return newWord;
 // user interface logic
 $(document).ready(function() {
   $("form#roman-numerals").submit(function(event) {
