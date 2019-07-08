8347c757bb64a418a326198c76e661a26f1fb21f
Soro Cat <sorousha326@gmail.com>
2019-07-08 11:02:03 -0700

    add js changes

--- js/script.js
+++ js/script.js
@@ -1,22 +1,24 @@
 var roman = function(sentence){
+  console.log("roman")
   var numerals = ["I","V","X","L","C","D","M"]
   var number = ["1","5","10","50","100","500","1000"]
   // var splitstring = sentence.split("");
-  var newWord;
-  for(x=0; x < splitstring.length; x++){
-    numerals.forEach(function(letter){
-     if (splitstring[0]===letter){
+  var newWord = sentence.split("");
+  for(x=0; x < numbers.length; x++){
+    console.log(x);
+     if (sentence===number[x]){
        console.log(x);
-     });
-    };
-  }
+     }
+   }
     return newWord;
 // user interface logic
 $(document).ready(function() {
-  $("form#roman-numerals").submit(function(event) {
+  $("form#roman").submit(function(event) {
+    console.log("roman")
     event.preventDefault();
     var words = $('#words').val()
     var splitstring = roman(words)
     $('#output').append(splitstring)
   })
-});
+ });
+}
