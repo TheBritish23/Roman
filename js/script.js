ce8565cf0b85febc31dc400ca2a36c679b26b221
Soro Cat <sorousha326@gmail.com>
2019-07-09 14:20:56 -0700

    add changes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 var roman = function(sentence){
   var romannumber
   var numerals = ["I","V","X","L","C","D","M"]
+  var numerals2 = ["I","II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+  var numerals3 = ["X", "XX", "XXX", "XL", "LX", "LXX","LXXX", "XC"]
+  var numerals4 = ["C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+  var numerals5 = ["M", "MM", "MMM", "MMMM", "MMMMM", "VM", "VMM", "VMMMM", "VMMMMM"]
   var number = ["1","5","10","50","100","500","1000"]
   // var splitstring = sentence.split("");
   var newWord = sentence.split("");
