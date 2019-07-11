09a4168402421efb435e0a592c3252d0f602ff3e
Soro Cat <sorousha326@gmail.com>
2019-07-11 14:58:20 -0700

    add changes

--- js/script.js
+++ js/script.js
@@ -8,20 +8,32 @@ var roman = function(sentence){
   var number = ["1","5","10","50","100","500","1000"]
   // var splitstring = sentence.split("");
   var newWord = sentence.split("");
-  for(x=0; x < number.length; x++){
-     if (sentence===number[x]){
-       console.log(numerals[x]);
-       romannumber=numerals[x]
-     }
-     }
-     if (newWord.length===4) {
-       romannumber.push(thousands[newWord[0]])
-       romannumber.push(hundreds[newWord[1]])
-       romannumber.push(tens[newWord[2]])
-       romannumber.push(ones[newWord[3]])
-       console.log(romannumber)
-   }
-    return romannumber;
+  // for(x=0; x < number.length; x++){
+  //    if (sentence===number[x]){
+  //      console.log(numerals[x]);
+  //      romannumber=numerals[x]
+  //    }
+  //    }
+  //    if (newWord.length===4) {
+  //      romannumber.push(thousands[newWord[0]])
+  //      romannumber.push(hundreds[newWord[1]])
+  //      romannumber.push(tens[newWord[2]])
+  //      romannumber.push(ones[newWord[3]])
+  //  }
+  //  if (newWord.length===3) {
+  //    romannumber.push(hundreds[newWord[0]])
+  //    romannumber.push(tens[newWord[1]])
+  //    romannumber.push(ones[newWord[2]])
+  //  }
+  //  if (newWord.length===2) {
+  //    romannumber.push(tens[newWord[0]])
+  //    romannumber.push(ones[newWord[1]])
+  //  }
+  //  if (newWord.length===1) {
+  //    romannumber.push(ones[newWord[0]])
+  //  }
+  //   return romannumber;
+   for(x=0,1,2,3;[newWord[4]])
   }
 
 // user interface logic
